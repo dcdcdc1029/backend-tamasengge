1bd54de03c51adc4c173115d744828b23fea4b09
dc <52826071+dcdcdc1029@users.noreply.github.com>
2022-10-25 19:36:21 +0700

    Update lives.js

--- src/routes/lives.js
+++ src/routes/lives.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
-const liveController = require('../controllers/live');
+const liveController = require('../controllers/lives');
 
 //get semua live music
 router.get("/", liveController.getAllLive);
