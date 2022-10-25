433c444ce49e1011cef689a561930edb280a0f83
dc <52826071+dcdcdc1029@users.noreply.github.com>
2022-10-25 19:35:26 +0700

    Create live_model.js

---
+++ src/models/live_model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LivePost = new Schema({
+    nama: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: Number,
+        required: true
+    },
+    harga: {
+        type: Number,
+        required: true
+    },
+    alamat: {
+        type: String,
+        required: true
+    },
+    deskripsi: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: Array,
+        required: true
+    },
+    author: {
+        type: Object,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("HotelPost", HotelPost);
